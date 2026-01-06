d0fde68965390494ac9ba45d08b6d9788ed9480c
Dennis Keßel <dennis.kessel@gmail.com>
2026-01-06 20:50:23 +0100

    Enhance bahnanzeige.py with interactive delay options
    Added interactive features for train delay information and compensation based on ticket price.

--- bahnanzeige.py
+++ bahnanzeige.py
@@ -1,3 +1,7 @@
+### Ursprüngliche Idee war eine Zuganzeige, da mir im Zug langweilig war.
+### Hier gibt es nun mehrere interaktive Möglichkeiten rund um die
+### Verspätung im ÖPNV
+
 import random
 import datetime
 from datetime import timedelta
@@ -8,6 +12,11 @@ jetzt = datetime.datetime.now()
 uhrzeit_text = jetzt.strftime("%H:%M") 
 print("Aktuelle Zeit:", uhrzeit_text)
 
+### Ticketpreis des Fahrgastes
+
+ticketpreis = input("Wieviel hast du für das Ticket bezahlt?")
+ticketpreis = int(ticketpreis)
+
 ### Verspaetungswürfel
 
 verspaetung = random.randint(0, 60)
@@ -15,6 +24,18 @@ verspaetung = random.randint(0, 60)
 ankunft_neu = jetzt + datetime.timedelta(minutes=verspaetung)
 print(f"Ihr Zug hat {verspaetung} Minuten Verspätung und kommt an um {ankunft_neu}")
 
+### Gutschrift, falls eine Verspätung auftritt
+ 
+if ticketpreis > 25 and verspaetung > 30:
+    gutschrift = ticketpreis / 15
+    print(f"Sie haben eine Gutschrift in Höhe von {gutschrift} erhalten.")
+else:
+    print(f"Sie haben keine Gutschrift erhalten, da sie die Bedingungen nicht erfüllen.")
+
 ### Gruende für die Verspaetung
 
+gruende = {"Wetter": "Die Witterungsbedingungen sind schlecht.",
+           "Fahrgäste": "Die Fahrgäste haben Mist gebaut."}
+
 ### Komplette Anzeige von allem
+
